b344dc37955d635469af4ab29fd05cabe377674d
Ivan BOHONOSIUK <ibohonos@e2r7p9.unit.ua>
2018-01-09 09:01:35 +0200

    some fixes in search & init modific

--- srcs/ft_find.c
+++ srcs/ft_find.c
@@ -6,7 +6,7 @@
 /*   By: ibohonos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 23:23:55 by ibohonos          #+#    #+#             */
-/*   Updated: 2017/12/31 21:01:25 by ibohonos         ###   ########.fr       */
+/*   Updated: 2018/01/09 08:55:46 by ibohonos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	ft_find(char *s, int i, char c)
 	int count;
 
 	count = 0;
-	while (s[i] != '\0' && ft_find_types(s[i], -1) != 1)
+	while (s[i] != '\0' && ft_find_types(s[i], -1) != 0
+		&& ft_find_types(s[i], -1) != 1)
 	{
 		if (s[i] == c)
 			count++;
--- srcs/ft_init_modific.c
+++ srcs/ft_init_modific.c
@@ -6,7 +6,7 @@
 /*   By: ibohonos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 17:57:39 by ibohonos          #+#    #+#             */
-/*   Updated: 2018/01/06 16:37:08 by ibohonos         ###   ########.fr       */
+/*   Updated: 2018/01/09 08:59:55 by ibohonos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,14 @@
 
 int	ft_init_modific(char *str, int i, t_struct *p)
 {
-	if (ft_find(str, i, 'h') / 2 != 1 && ft_find(str, i, 'h') != 0
-		&& p->spec_hh == 0)
+	if (ft_strstr(str, "z"))
 	{
-		p->spec_h = 1;
+		p->spec_z = 1;
 		i++;
 	}
-	else if (ft_find(str, i, 'h') / 2 == 1 && ft_find(str, i, 'h') != 0)
+	else if (ft_strstr(str, "j"))
 	{
-		p->spec_h = 0;
-		p->spec_hh = 1;
-		i += 2;
-	}
-	if (ft_find(str, i, 'l') / 2 != 1 && ft_find(str, i, 'l') != 0
-		&& p->spec_ll == 0)
-	{
-		p->spec_l = 1;
+		p->spec_j = 1;
 		i++;
 	}
 	else if (ft_find(str, i, 'l') / 2 == 1 && ft_find(str, i, 'l') != 0)
@@ -38,14 +30,22 @@ int	ft_init_modific(char *str, int i, t_struct *p)
 		p->spec_ll = 1;
 		i += 2;
 	}
-	if (ft_strstr(str, "j"))
+	else if (ft_find(str, i, 'l') / 2 != 1 && ft_find(str, i, 'l') != 0
+		&& p->spec_ll == 0)
 	{
-		p->spec_j = 1;
+		p->spec_l = 1;
 		i++;
 	}
-	if (ft_strstr(str, "z"))
+	else if (ft_find(str, i, 'h') / 2 == 1 && ft_find(str, i, 'h') != 0)
 	{
-		p->spec_z = 1;
+		p->spec_h = 0;
+		p->spec_hh = 1;
+		i += 2;
+	}
+	else if (ft_find(str, i, 'h') / 2 != 1 && ft_find(str, i, 'h') != 0
+		&& p->spec_hh == 0)
+	{
+		p->spec_h = 1;
 		i++;
 	}
 	return (i);
